d33adcdea3720a364a81bca9961c6b7685c45c21
Fabian Schwarzkopf <f.schwarzkopf@yahoo.de>
2022-12-20 22:54:10 +0100

    Updated meal-planner

---
+++ meal-planner/sw.js
@@ -0,0 +1,114 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-6e567876'], (function (workbox) { 'use strict';
+
+  self.addEventListener('message', event => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+      self.skipWaiting();
+    }
+  });
+
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+  workbox.precacheAndRoute([{
+    "url": "assets/index.35b815c0.css",
+    "revision": null
+  }, {
+    "url": "assets/index.eba2bed5.js",
+    "revision": null
+  }, {
+    "url": "assets/webfontloader.b777d690.js",
+    "revision": null
+  }, {
+    "url": "assets/workbox-window.prod.es5.d2780aeb.js",
+    "revision": null
+  }, {
+    "url": "index.html",
+    "revision": "3b3208b7fda6527bfa3093dd8bc3a1a7"
+  }, {
+    "url": "montserrat/montserrat.css",
+    "revision": "e0db66cfcd709a3451a179ce1c9cfb4a"
+  }, {
+    "url": "android-chrome-192x192.png",
+    "revision": "26fa90d90d5a50199ca9bd525e5b2a2e"
+  }, {
+    "url": "android-chrome-512x512.png",
+    "revision": "9c8f62cd3a227602501f92ef94bd895f"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "a7c7ff1c692c3344e96a6a5f7431af3c"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+
+}));
